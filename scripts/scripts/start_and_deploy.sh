b22eb6f8db017b2ca9481c2362f7593d4776ea97
Kimdongkyu <151708233+owencity@users.noreply.github.com>
2024-09-06 03:29:48 +0900

    Delete scripts/scripts/start_and_deploy.sh

--- scripts/scripts/start_and_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the application
-echo "Starting the application..."
-bash scripts/start.sh
-
-# Deploy or run additional deployment scripts
-echo "Running the deployment script..."
-bash deploy.sh
